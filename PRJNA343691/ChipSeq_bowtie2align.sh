1a19f285d7d50394595991e6b58a03464706a0c2
Shruti Singh Kakan <singhkak@usc.edu>
2025-10-14 04:01:08 -0700

    Corrected samtools view parameters

--- PRJNA343691/ChipSeq_bowtie2align.sh
+++ PRJNA343691/ChipSeq_bowtie2align.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #SBATCH -p bigmem
-#SBATCH --time=03:00:00
+#SBATCH --time=23:00:00
 #SBATCH --mem=256GB
 #SBATCH -c 8
 #SBATCH --ntasks=4
@@ -9,6 +9,7 @@ module load devel
 module load system
 module load biology
 module load bowtie2/2.5.4
+module load samtools
 
 index=/home/groups/ximenac/XCD_BSSeq/GSE134873/mouse_genome_no_patches/Bowtie2_mm39/Bowtie2_mm39
 fastq="/scratch/groups/ximenac/PRJNA343691/ChipSeq_P21/fastq_trimmed"
@@ -22,8 +23,18 @@ do
     out="$Output"
     cd $out
     outname="${j%%.*}"
-    bowtie2 -q -N 1 --very-sensitive -p 8 -x $index $fastq/"$j" -S "$outname".sam 
-    samtools view -bS "$outname".sam tmp.bam #convert sam to bam
+    bowtie2 -q -N 1 --very-sensitive -p 8 -x $index $fastq/"$j" -S "$outname".sam &
+    cd ..
+done
+
+wait
+
+for j in *sam
+do
+    out="$Output"
+    cd $out
+    outname="${j%%.*}" 
+    samtools view -bS -o tmp.bam "$outname".sam #convert sam to bam
     samtools view -bSq 20 tmp.bam | samtools sort -@ 8 -o "$outname".bam #filter reads with mapq > 20
     samtools index -b "$outname".bam &
     rm tmp.bam
